60e4fac17ab34ec80b24bdad93bb44606d6d769f
YAKMESH <io@yakmesh.dev>
2026-02-03 01:34:57 -0600

    Add yakmesh.config.js to gitignore (use example instead)

--- yakmesh.config.js
+++
@@ -1,8 +0,0 @@
-// Alpha node - connects to Gamma on LAN
-export default {
-  nodeId: 'alpha-lan-test',
-  server: { port: 3001, host: '0.0.0.0' },
-  mesh: { port: 9002, host: '0.0.0.0' },
-  peers: ['ws://192.168.1.178:9001'],
-  dataDir: './test-nodes/data-alpha'
-};
